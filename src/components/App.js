4445e6b7c5f5e150a36235132f91068df2652d10
chandni-tilasi <96279259+chandni-tilasi@users.noreply.github.com>
2023-01-25 15:56:13 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,23 +1,30 @@
-import React, {useState} from 'react'
-import '../styles/App.css';
+import React, { useState } from "react";
+import "../styles/App.css";
 const App = () => {
-//code here 
-  
+  //code here
 
-  
+  const [on, seton] = useState(false);
+  const [off, setoff] = useState(false);
+
+  function handleClick() {
+    seton(!on);
+    setoff(!off);
+  }
+
   return (
     <div className="App">
       <button id="btn1" onClick={handleClick}>
-      {/* assign value for button 1 */}
+        {/* assign value for button 1 */}
+        {on ? "on" : "off"}
       </button>
       <br />
       <br />
       <button id="btn2" onClick={handleClick}>
         {/* assign value for button 2 */}
+        {off ? "off" : "on"}
       </button>
     </div>
   );
-}
-
+};
 
 export default App;
